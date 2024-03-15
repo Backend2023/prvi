2951d0c226937047523c93b28732f7b7a65ad3e7
mrvic <mrvic.predrag@gmail.com>
2024-03-15 20:26:24 +0100

    Nas cutom iterator

--- src/Primjeri/Primjer7/PrimjerStaticFactory.php
+++ src/Primjeri/Primjer7/PrimjerStaticFactory.php
@@ -2,18 +2,53 @@
 namespace pmrvic\prvi\Primjeri\Primjer7;
 include "./../../../vendor/autoload.php";
 
-use DesignPatterns\Creational\SimpleFactory\SimpleFactory;
-use DesignPatterns\Creational\SimpleFactory\Bicycle;
+/*
+Za razliku od simple factory, static koristi static metodu, 
+dakle ne treba kreirati objekt tipa factory
+*/
 
-$b1=new Bicycle();
+use DesignPatterns\Creational\StaticFactory\StaticFactory;
+use Exception;
+use InvalidArgumentException;
+//use Exception;
+//use DesignPatterns\Creational\StaticFactory\FormatNumber;
+//use DesignPatterns\Creational\StaticFactory\FormatString;
+//use DesignPatterns\Creational\StaticFactory\Formatter;
 
-$b1->driveTo('more');
 
-$b2=(new SimpleFactory())->createBicycle();  // kreira objekt tipa "Bicycle"
-$b2->driveTo('more');
+$broj1=StaticFactory::factory('number');
+
+$tekst1=StaticFactory::factory('string');
 
 
 
+try {
+    $greska1=StaticFactory::factory('int');
+} 
+catch (InvalidArgumentException $exc) {
+
+    echo PHP_EOL."mozes unijeti samo 'number'  ili 'string'";
+}
+catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+} finally {
+    echo PHP_EOL."evo sve sam rijesio, exception je uhvaćen!";
+}
+
+// Zelim kreirati 100 objekata tipa number...
+
+/*
+ * 
+ * $broj1=StaticFactory::factory('number');
+ * ...
+ * $broj100=StaticFactory::factory('number');
+ * 
+ */
+$stobrojeva=[];
+
+// Primjer masovnog kreiranja objekata:
+$stobrojeva=StaticFactory::factoryMassProduce('number',100);
+var_dump($stobrojeva);
 
 
 
---
+++ src/Primjeri/Primjer7/primjerIteratorBook.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace pmrvic\prvi\Primjeri\Primjer7;
+
+include "./../../../vendor/autoload.php";
+
+use DesignPatterns\Behavioral\Iterator\Book;
+use DesignPatterns\Behavioral\Iterator\BookList;
+use DesignPatterns\Behavioral\Iterator\PolicaSKnjigama;
+use \DesignPatterns\Behavioral\Iterator\BookFactory;
+
+$p1 = new PolicaSKnjigama(BookFactory::napraviHrpuKnjiga(77));
+//var_dump($p1);
+foreach ($p1->getPolica() as $k) {  // ovo nije dobar princip, 
+    //  echo $k->getAuthorAndTitle();
+      //var_dump($k);
+}
+
+//var_dump($p1);
+echo "test";
+// kreiraj factory s knigama
+//TODO  ne radi foreach petlja, 
+
+
+
+// KORISIMO ORIGINAL
+
+$bookList = new BookList();
+$bookList->addBook(new Book('Learning PHP Design Patterns', 'William Sanders'));
+$bookList->addBook(new Book('Professional Php Design Patterns', 'Aaron Saray'));
+$bookList->addBook(new Book('Clean Code', 'Robert C. Martin'));
+
+$books = [];
+
+foreach ($bookList as $book) {
+      echo $book->getAuthorAndTitle();
+}
